be9365264d8a06d0defe847ead8ece9454ef0fc7
Rohit Gutal <gutalrohit67@gmail.com>
2021-10-10 15:38:15 +0200

    add new project

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '344fb99a44794a905d5fcaaa107d5675'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'fd47e02a-dbc4-4105-abb3-c9d2a6f5a35a'
+};
